0de9711d1ecfe5c9680e7b1a5ebe8118b8c24e30
Andres Freyria <andres.freyria@gmail.com>
2012-12-26 17:35:00 -0600

    Compatibility for rails 3.1 and mongoid 2.4.10

--- lib/mongo_session_store/mongoid.rb
+++ lib/mongo_session_store/mongoid.rb
@@ -8,6 +8,10 @@ module ActionDispatch
         include Mongoid::Document
         include Mongoid::Timestamps
 
+        store_in :sessions
+
+        identity :type => String
+
         field :data, :type => String, :default => [Marshal.dump({})].pack("m*")
         index :updated_at
       end
@@ -16,44 +20,59 @@ module ActionDispatch
       cattr_accessor :session_class
       self.session_class = Session
 
-      SESSION_RECORD_KEY = 'rack.session.record'.freeze
+      SESSION_RECORD_KEY = 'rack.session.record'
+      ENV_SESSION_OPTIONS_KEY = Rack::Session::Abstract::ENV_SESSION_OPTIONS_KEY if ::Rails.version >= "3.1"
 
-      private
-        def generate_sid
-          BSON::ObjectId.new
-        end
+      def get_session(env, sid)
+        sid ||= generate_sid
+        session = find_session(sid)
+        env[SESSION_RECORD_KEY] = session
+        [sid, unpack(session.data)]
+      end
 
-        def get_session(env, sid)
-          sid ||= generate_sid
-          session = find_session(sid)
-          env[SESSION_RECORD_KEY] = session
-          [sid, unpack(session.data)]
-        end
-
-        def set_session(env, sid, session_data)
-          record = env[SESSION_RECORD_KEY] ||= find_session(sid)
-          record.data = pack(session_data)
-          # Rack spec dictates that set_session should return true or false
-          # depending on whether or not the session was saved or not.
-          # However, ActionPack seems to want a session id instead.
-          record.save ? sid : false
-        end
-
-        def find_session(id)
-          id = BSON::ObjectId.from_string(id.to_s)
-          @@session_class.first(:conditions => { :_id => id }) ||
-            @@session_class.new(:id => id)
-        end
-
-        def pack(data)
-          [Marshal.dump(data)].pack("m*")
-        end
-
-        def unpack(packed)
-          return nil unless packed
-          Marshal.load(packed.unpack("m*").first)
-        end
+      def set_session(env, sid, session_data, options = nil)
+        record = get_session_model(env, sid)
+        record.data = pack(session_data)
+        
+        # Rack spec dictates that set_session should return true or false
+        # depending on whether or not the session was saved or not.
+        # However, ActionPack seems to want a session id instead.
+        record.save ? sid : false
+      end
       
+      def find_session(id)
+        @@session_class.find_or_create_by(:id => id)
+      end
+      
+      def destroy(env)
+        destroy_session(env, current_session_id(env), {})
+      end
+        
+      def destroy_session(env, session_id, options)
+        if sid = current_session_id(env)
+          get_session_model(env, sid).destroy
+          env[SESSION_RECORD_KEY] = nil
+        end
+        
+        generate_sid unless options[:drop]
+      end
+      
+      def get_session_model(env, sid)
+        if env[ENV_SESSION_OPTIONS_KEY][:id].nil?
+          env[SESSION_RECORD_KEY] = find_session(sid)
+        else
+          env[SESSION_RECORD_KEY] ||= find_session(sid)
+        end
+      end
+
+      def pack(data)
+        [Marshal.dump(data)].pack("m*")
+      end
+      
+      def unpack(packed)
+        return nil unless packed
+        Marshal.load(packed.unpack("m*").first)
+      end
     end
   end
 end
